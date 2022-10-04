3116b6b8d1eb90060b7a60d07c30908fe91647d2
theoriginalmina <mina_daoud@outlook.com>
2022-10-04 03:22:04 +0200

    basic authentication

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+
+// type mina = {
+//     session: any;
+//     Component: any;
+//     pageProps: any;
+// };
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+    const { data } = useSession();
+
+    console.log("data", data);
+
+    return (
+        <div>
+            {data?.user ? (
+                <button onClick={() => signOut()}>Sign Out</button>
+            ) : (
+                <button onClick={() => signIn("google")}>Sign in</button>
+            )}
+
+            {data?.user?.name}
+        </div>
+    );
+};
+
+export default Home;
